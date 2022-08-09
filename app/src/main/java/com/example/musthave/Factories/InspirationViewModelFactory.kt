e45c9b285a6492b7673fb9111b39d06abe7d8c72
Guillermo Van Gool <guivangool@gmail.com>
2022-08-09 06:32:51 -0300

    Version with MVVM Arq Pattern

---
+++ app/src/main/java/com/example/musthave/Factories/InspirationViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.example.musthave.Factories
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.musthave.Repositories.InspirationRepository
+import com.example.musthave.Repositories.ObstacleRepository
+import com.example.musthave.viewModels.InspirationViewModel
+import com.example.musthave.viewModels.ObstacleViewModel
+
+class InspirationViewModelFactory (
+    private val repository: InspirationRepository)
+    : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if(modelClass.isAssignableFrom(InspirationViewModel::class.java)){
+                return InspirationViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown View Model class")
+        }
+}
---
+++ app/src/main/java/com/example/musthave/viewModels/ObstacleViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.musthave.viewModels
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.musthave.DataEntities.ObstacleEntity
+import com.example.musthave.Repositories.ObstacleRepository
+import kotlinx.coroutines.launch
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ObstacleViewModel (private val repository : ObstacleRepository) : ViewModel() {
+    val description = MutableLiveData<String>()
+    var year : MutableLiveData<Int> = MutableLiveData()
+    var month : MutableLiveData<Int> = MutableLiveData()
+    var day : MutableLiveData<Int> = MutableLiveData()
+
+    init {
+        //Set today as default value
+        val calendar: Calendar = Calendar.getInstance()
+        year.value = calendar.get(Calendar.YEAR)
+        month.value = calendar.get(Calendar.MONTH)
+        day.value = calendar.get(Calendar.DAY_OF_MONTH)
+    }
+    //The fun Insert receives a callback function as parameter to execute it after the INSERT and
+    //notifies the Activity.
+    fun insert(callback : () -> Unit){
+
+        //Convert year,month and day TO String and Date
+        var calendar = Calendar.getInstance()
+        calendar.set(year.value!!, month.value!!, day.value!!)
+
+        var dateString =  SimpleDateFormat(
+            "yyyyMMdd",
+            Locale.getDefault()
+        ).format(calendar.time)
+
+        var dateDate = calendar.time
+
+        viewModelScope.launch {
+            repository.insert(ObstacleEntity(null, description.value!!, dateString, dateDate, 0))
+            callback()
+        }
+    }
+}
