cf6873f5e80f1acd107c4ba4bef6f20e7e8665af
Guillermo Van Gool <guivangool@gmail.com>
2023-01-26 07:06:53 -0300

    26012023

--- app/src/main/java/com/example/musthave/Activities/Initial.kt
+++ app/src/main/java/com/example/musthave/Activities/Initial.kt
@@ -7,11 +7,20 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.postDelayed
 import com.example.musthave.R
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class Initial : AppCompatActivity() {
+    private lateinit var analytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
+        val eventBundle = Bundle()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial)
+
+        analytics = Firebase.analytics
+        eventBundle.putString(FirebaseAnalytics.Param.LOCATION,"MDQ")
+        analytics.logEvent(FirebaseAnalytics.Event.APP_OPEN,eventBundle)
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
