554647bee74e9c65eb994f5f9997ca60b95a3562
Guillermo Van Gool <guivangool@gmail.com>
2023-08-30 13:40:52 -0300

    Add Fragment to Create Inspiration feature

--- app/src/main/java/com/example/musthave/Activities/RemoveObstacle.kt
+++ app/src/main/java/com/example/musthave/Activities/RemoveObstacle.kt
@@ -9,9 +9,11 @@ import com.example.musthave.Repositories.ObstacleRepository
 import com.example.musthave.databinding.ActivityRemoveObstacleBinding
 import com.example.musthave.viewModels.ObstacleViewModel
 import com.example.musthave.Factories.ObstacleViewModelFactory
+import com.example.musthave.Fragments.AcceptCancel
+import com.example.musthave.Interfaces.OnAcceptCancelButtonClickListener
 import com.example.musthave.R
 
-class RemoveObstacle : AppCompatActivity() {
+class RemoveObstacle : AppCompatActivity(),OnAcceptCancelButtonClickListener{
     var binding: ActivityRemoveObstacleBinding? = null
     private lateinit var obstacleViewModel: ObstacleViewModel
 
@@ -41,20 +43,32 @@ class RemoveObstacle : AppCompatActivity() {
         //Set minium to the DatePicker
         binding?.dpObstacle?.setMinDate(System.currentTimeMillis())
 
-        binding?.btnCancel?.setOnClickListener {
-            finish()
-        }
-        binding?.btnAccept?.setOnClickListener {
-            if (binding?.etSetObstacleName?.text?.toString() != "") {
-                //Send a callback function by parameter to be executed after the INSERT
-                obstacleViewModel.insert({ finish() })
-            } else {
-                Toast.makeText(
-                    this,
-                    getString(R.string.CREATE_OBSTACLE_SET_FIELDS_VALIDATION),
-                    Toast.LENGTH_LONG
-                ).show()
-            }
+        //Add the fragment AcceptCancel programmatically
+        if (savedInstanceState == null) {
+            val fragment = AcceptCancel()
+            val fragmentTransaction  = supportFragmentManager.beginTransaction()
+            fragment.setOnAcceptCancelButtonClickListener(this)
+            fragmentTransaction.add(R.id.fragment_accept_cancel,fragment)
+            fragmentTransaction.commit()
         }
     }
+
+    override fun onAcceptButtonCLicked() {
+        if (binding?.etSetObstacleName?.text?.toString() != "") {
+            //Send a callback function by parameter to be executed after the INSERT
+            obstacleViewModel.insert({ finish() })
+        } else {
+            Toast.makeText(
+                this,
+                getString(R.string.CREATE_OBSTACLE_SET_FIELDS_VALIDATION),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    override fun onCancelButtonCLicked() {
+       finish()
+    }
+
+
 }
